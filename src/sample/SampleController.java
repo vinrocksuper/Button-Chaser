0b18834b2c3d3a7bcf18c85bebf3cda4640e2323
BT_1N3_04 <vli8558@bths.edu>
2019-01-02 10:03:14 -0500

    I have no idea what I'm doing

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -4,12 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
 public class SampleController {
-    public Label helloWorld;
+
     public Label score;
     private int count =0;
-    public void sayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello World!");
-    }
+
 
     public void increment(ActionEvent actionEvent) {
         count++;
