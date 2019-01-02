f4b1f1787d53568d08e7ec508126fd93c6d4e813
BT_1N3_04 <vli8558@bths.edu>
2019-01-02 10:13:52 -0500

    I have no idea what I'm doing

--- src/sample/GameCode.java
+++ src/sample/GameCode.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 public class GameCode {
+    private String[] colorList;
+    private int diff = 1;
+    public String[] genColor(){
+        for(int i=0;i<diff;i++)
+        {
+            int temp = (int)(Math.random()*4);
+            if(temp == 0){
+                colorList[i] = "red";
+            }
+            if(temp == 1){
+                colorList[i] = "yellow";
+            }
+            if(temp == 2){
+                colorList[i] = "blue";
+            }
+            else{
+                colorList[i] = "green";
+            }
+        }
+        return colorList;
+    }
+    public void changeColor(){
+
+    }
+
 }
