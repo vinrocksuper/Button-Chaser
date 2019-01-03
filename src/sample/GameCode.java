fd019e2c3ac3786694ac5c4b725d9dcdfce46804
BT_1N3_04 <vli8558@bths.edu>
2019-01-03 10:06:44 -0500

    I want 2 die

--- src/sample/GameCode.java
+++ src/sample/GameCode.java
@@ -1,31 +1,101 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-public class GameCode {
-    private String[] colorList;
+import javafx.animation.AnimationTimer;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class GameCode implements Initializable {
+  /**  private ArrayList<String> colorList = new ArrayList<>();
     private int diff = 1;
-    public String[] genColor(){
+    public Button a;
+    private String[] constants = {"#ff0000","#ffff00","#0000ff","#00ff00"};
+    public void genColor(){
         for(int i=0;i<diff;i++)
         {
             int temp = (int)(Math.random()*4);
             if(temp == 0){
-                colorList[i] = "red";
+                colorList.set(i,"red");
             }
             if(temp == 1){
-                colorList[i] = "yellow";
+                colorList.set(i,"yellow");
             }
             if(temp == 2){
-                colorList[i] = "blue";
+                colorList.set(i,"blue");
             }
             else{
-                colorList[i] = "green";
+                colorList.set(i,"green");
             }
         }
-        return colorList;
+
     }
     public void changeColor(){
+        genColor();
+        for(int i=0;i<colorList.size();i++)
+        {
+            if(colorList.get(i).equals("red"))
+            {
+                a.setStyle("-fx-background-color:"+constants[0]);
+            }
+            if(colorList.get(i).equals("yellow"))
+            {
+                a.setStyle("-fx-background-color:"+constants[1]);
+            }
+            if(colorList.get(i).equals("blue"))
+            {
+                a.setStyle("-fx-background-color:"+constants[2]);
+            }
+            if(colorList.get(i).equals("green"))
+            {
+                a.setStyle("-fx-background-color:"+constants[3]);
+            }
+        }
+    } */
+
+  public Label score;
+  public Button a;
+  public Button b;
+    private int count =0;
+    private boolean counting = true;
+
+
+    public void increment(ActionEvent actionEvent) {
+        if(counting) {
+            count++;
+            score.setText("" + count);
+        }
+
+    }
+    public void switchPg()
+    {
+        Main.window.setScene(Main.s2);
 
     }
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        {
+            long step = System.nanoTime() + 10000000000L;
+            new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    if(now > step)
+                    {
+                        counting = false;
+                        score.setText("You ended with " + count);
+                        b.setVisible(true);
+                    }
+                }
+            }.start();
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    static Stage window;
+    static Scene s1;
+    static Scene s2;
 
-    @Override
+
+
+   @Override
     public void start(Stage primaryStage) throws Exception{
+       window = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+        window.setTitle("Hello World");
+        s1 = new Scene(root, 300, 275);
+        window.setScene(s1);
+        window.show();
 
+
+       Button x = new Button("Return");
+
+       x.setOnAction(e -> window.setScene(s1));
+       Label l = new Label("HIGH SCORES");
+       VBox l2 = new VBox();
+
+        l2.getChildren().addAll(l,x);
+
+        s2 = new Scene(l2,300,300);
+    }
 
     public static void main(String[] args) {
         launch(args);
